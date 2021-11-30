ff5a2baba35fc32588da4a198b1b07dd91f7bcca
toroddottestad <79814135+toroddottestad@users.noreply.github.com>
2021-11-30 14:58:53 +0100

    Oppdatert med .py-fil

---
+++ Trinnskatt/trinnskatt.py
@@ -0,0 +1,16 @@
+inntekt = float(input("Oppgje inntekt i kroner:"))
+
+grense_1 = 184800
+grense_2 = 260100
+... # Her manglar det kode
+... # Her manglar det kode
+
+skatt_1 = 0.017
+... # Her manglar det kode
+... # Her manglar det kode
+
+if inntekt < grense_1:
+    trinnskatt = 0
+elif inntekt < grense_2: 
+    trinnskatt = (inntekt - grense_1) * skatt_1
+elif ... # Her manglar det fleire linjer med kode
